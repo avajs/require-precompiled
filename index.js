0de15ac1da2e8d6295e36074098810375bbd7753
gloryofrobots <gloryofrobots@gmail.com>
2020-02-03 14:53:28 +0200

    fixes issue when there is no previous require handler in require.extensions this problem emerges particularly when dealing with .cjs files

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 module.exports = install;
 
+// we need to save it before it gets overriden by install
+const defaultHandler = require.extensions['.js'];
+
 function install(precompile, ext, extensions) {
 	ext = ext || '.js';
 	extensions = extensions || require.extensions;
 
-	var oldExtension = extensions[ext];
+	// if there is no handler in extensions(as in case with .cjs)
+	// use the default one which we saved before
+	var oldExtension = extensions[ext] || defaultHandler;
 
 	extensions[ext] = function (module, filename) {
 		var source = precompile(filename);
